302fd3ee1bd8eb2369ba6982496de0601e5dabab
omaximus <maxomelch93@gmail.com>
2015-02-23 15:34:30 +0200

    input binding logic changed

--- src/Field.php
+++ src/Field.php
@@ -8,104 +8,125 @@ use samson\activerecord\dbRelation;
  *
  */
 class Field extends \samson\core\CompressableExternalModule implements \samson\core\iModuleViewable
-{		
-	/** Database object classname which connected  with this field */
-	protected $entity;
-	
-	/** Database object field name */
-	protected $param;
-	
-	/** Database object current field value */
-	protected $value;
-	
-	/** Main field action controller */
-	protected $action = 'save';
-	
-	/** Special CSS classname for nested field objects to bind JS and CSS */
-	protected $cssclass = '__textarea';
-	
-	/** 
-	 * Pointer to database object instance 
-	 * @var \samson\activerecord\dbRecord
-	 * @see \samson\activerecord\dbRecord 
-	 */
-	protected $obj;
-	
-	/** Path to view file for field rendering */
-	protected $default_view = "index";
-	
-	/** Path to view file for inner field rendering */
-	protected $field_view = "field";
-	
-	/**
-	 * Create instance of field from metadata
-	 * @param unknown $entity
-	 * @param unknown $param
-	 * @param unknown $identifier
-	 */
-	public static function & fromMetadata( $entity, $param, $identifier, $classname = __CLASS__ )
-	{
-		$o = null;
-		
-		// Correct namespace classname generation 
-		$entity = ns_classname( $entity, __NAMESPACE__ );		
-		if( !class_exists( $entity ) ) e('Cannot create ## object instance - Class ## does not exists', E_SAMSON_CORE_ERROR, array(__CLASS__, $entity) );
-					
-		// Generate correct namespace for class
-		$classname = uni_classname(ns_classname( strtolower($classname), __NAMESPACE__ ));				
-		
-		// Try to get field module instance from core
-		if( null !== ($f = & m( $classname )) )
-		{			
-			// Try to find field corresponding entity
-			if( dbQuery( $entity )->id( $identifier )->first( $obj ) )
-			{		
-				// Create field object copy
-				$o = & $f->copy();
-				$o->view_path = $f->view_path;
-				$o->entity	= $entity;
-				$o->obj 	= & $obj;
-				$o->param 	= $param;
-				$o->value 	= $obj[ $param ];
-			}
-			else e('Cannot create ## object instance - Entity ## with id: ## - does not exists', E_SAMSON_CORE_ERROR, array(__CLASS__, $entity, $identifier) );
-		}
-		else e('Cannot create ## object instance - Field module ## not loaded to system core', E_SAMSON_CORE_ERROR, array(__CLASS__,$classname) );
-		
-		return $o;		
-	}
-	
-	/**
-	 * Create instance from object 
-	 * @param mixed 	$obj 	Object for creating inputfield
-	 * @param string 	$param	Object field name
-	 * @return \samson\cms\input\InputField Class instance
-	 */
-	public static function & fromObject( & $obj, $param, $classname = __CLASS__ )
-	{
-		$o = null;
-		
-		// If object is passed
-		if( !is_object( $obj ) ) e('Cannot create ## object instance - no object is passed', E_SAMSON_CORE_ERROR, __CLASS__ );
+{
+    /** Database object classname which connected  with this field */
+    protected $entity;
 
-		// Generate correct namespace for class
-		$classname = \samson\core\AutoLoader::oldClassName(__NAMESPACE__.'\\'.$classname );
+    /** Database object field name */
+    protected $param;
 
-		// Try to get field module instance from core
-		if( null !== ($f = & m( $classname )) )
-		{
-			// Create input field instance		
-			$o = & $f->copy();
-			$o->view_path = $f->view_path;
-			$o->entity	= get_class($obj);
-			$o->obj 	= & $obj;
-			$o->param 	= $param;
-			$o->value 	= $obj[ $param ];			
-		}	
-		else e('Cannot create ## object instance - Field module ## not loaded to system core', E_SAMSON_CORE_ERROR, array(__CLASS__,$classname) );
-		
-		return $o;
-	}
+    /** Database object current field value */
+    protected $value;
+
+    /** Main field action controller */
+    protected $action = 'save';
+
+    /** Special CSS classname for nested field objects to bind JS and CSS */
+    protected $cssclass = '__textarea';
+
+    /**
+     * Pointer to database object instance
+     * @var \samson\activerecord\dbRecord
+     * @see \samson\activerecord\dbRecord
+     */
+    protected $obj;
+
+    /** Path to view file for field rendering */
+    protected $default_view = "index";
+
+    /** Path to view file for inner field rendering */
+    protected $field_view = "field";
+
+    /**
+     * Create instance of field from metadata
+     * @param unknown $entity
+     * @param unknown $param
+     * @param unknown $identifier
+     * @return object
+     */
+    public static function & fromMetadata($entity, $param, $identifier, $classname = __CLASS__)
+    {
+        $o = null;
+
+        // Correct namespace classname generation
+        $entity = ns_classname($entity, __NAMESPACE__);
+        if (!class_exists($entity)) {
+            e(
+                'Cannot create ## object instance - Class ## does not exists',
+                E_SAMSON_CORE_ERROR,
+                array(__CLASS__, $entity)
+            );
+        }
+
+        // Generate correct namespace for class
+        $classname = uni_classname(ns_classname(strtolower($classname), __NAMESPACE__));
+
+        // Try to get field module instance from core
+        if (null !== ($f = & m($classname))) {
+            // Try to find field corresponding entity
+            if (dbQuery($entity)->id($identifier)->first($obj)) {
+                // Create field object copy
+                $o = & $f->copy();
+                $o->view_path = $f->view_path;
+                $o->entity	= $entity;
+                $o->obj 	= & $obj;
+                $o->param 	= $param;
+                $o->value 	= $obj[$param];
+            } else {
+                e(
+                    'Cannot create ## object instance - Entity ## with id: ## - does not exists',
+                    E_SAMSON_CORE_ERROR,
+                    array(__CLASS__, $entity, $identifier)
+                );
+            }
+        } else {
+            e(
+                'Cannot create ## object instance - Field module ## not loaded to system core',
+                E_SAMSON_CORE_ERROR,
+                array(__CLASS__,$classname)
+            );
+        }
+
+        return $o;
+    }
+
+    /**
+     * Create instance from object
+     * @param mixed 	$obj 	Object for creating inputfield
+     * @param string 	$param	Object field name
+     * @return \samson\cms\input\InputField Class instance
+     */
+    public static function & fromObject(& $obj, $param, $classname = __CLASS__)
+    {
+        $o = null;
+
+        // If object is passed
+        if (!is_object($obj)) {
+            e('Cannot create ## object instance - no object is passed', E_SAMSON_CORE_ERROR, __CLASS__);
+        }
+
+        // Generate correct namespace for class
+        $classname = \samson\core\AutoLoader::oldClassName(__NAMESPACE__.'\\'.$classname);
+
+        // Try to get field module instance from core
+        if (null !== ($f = & m($classname))) {
+            // Create input field instance
+            $o = & $f->copy();
+            $o->view_path = $f->view_path;
+            $o->entity	= get_class($obj);
+            $o->obj 	= & $obj;
+            $o->param 	= $param;
+            $o->value 	= $obj[$param];
+        } else {
+            e(
+                'Cannot create ## object instance - Field module ## not loaded to system core',
+                E_SAMSON_CORE_ERROR,
+                array(__CLASS__,$classname)
+            );
+        }
+
+        return $o;
+    }
 
     /**
      * Special function for processing value before saving
@@ -114,36 +135,42 @@ class Field extends \samson\core\CompressableExternalModule implements \samson\c
     {
         return $input;
     }
-	
-	// Controller 
-	
-	/**
-	 * Save input field value
-	 * @param mixed $value Field value
-	 */
-	public function __save()
-	{
-		// Does it necessary?
-		s()->async(true);	
 
-		// If we have post data
-		if( isset($_POST) )
-		{
-			// Make pointers to posted parameters
-			$entity = & $_POST['__entity'];
-			$param 	= & $_POST['__param'];
-			$id 	= & $_POST['__obj_id'];
-			$value 	= & $_POST['__value'];
-			
-			// Check if all nessesarly data is passed
-			if( !isset( $value ))	return e('CMSField - no "value" is passed for saving', E_SAMSON_CORE_ERROR);
-			if( !isset( $entity )) 	return e('CMSField - no "entity" is passed for saving', E_SAMSON_CORE_ERROR);
-			if( !isset( $id )) 		return e('CMSField - no "object identifier" is passed for saving', E_SAMSON_CORE_ERROR);
-			if( !isset( $param )) 	return e('CMSField - no "object field name" is passed for saving', E_SAMSON_CORE_ERROR);
-			
-			// Try to find passed object for saving
-			if( dbQuery( $entity )->id( $id )->first( $obj ) )
-			{
+    // Controller
+
+    /**
+     * Save input field value
+     * @param mixed $value Field value
+     */
+    public function __save()
+    {
+        // Does it necessary?
+        s()->async(true);
+
+        // If we have post data
+        if (isset($_POST)) {
+            // Make pointers to posted parameters
+            $entity = & $_POST['__entity'];
+            $param 	= & $_POST['__param'];
+            $id 	= & $_POST['__obj_id'];
+            $value 	= & $_POST['__value'];
+
+            // Check if all nessesarly data is passed
+            if (!isset($value)) {
+                return e('CMSField - no "value" is passed for saving', E_SAMSON_CORE_ERROR);
+            }
+            if (!isset($entity)) {
+                return e('CMSField - no "entity" is passed for saving', E_SAMSON_CORE_ERROR);
+            }
+            if (!isset($id)) {
+                return e('CMSField - no "object identifier" is passed for saving', E_SAMSON_CORE_ERROR);
+            }
+            if (!isset($param)) {
+                return e('CMSField - no "object field name" is passed for saving', E_SAMSON_CORE_ERROR);
+            }
+
+            // Try to find passed object for saving
+            if (dbQuery($entity)->id($id)->first($obj)) {
                 // If our object is material field
                 if ($obj instanceof \samson\activerecord\materialfield) {
                     // Get current material
@@ -155,7 +182,13 @@ class Field extends \samson\core\CompressableExternalModule implements \samson\c
 
                         // For each child create or update material field record
                         foreach ($children as $child) {
-                            if (!dbQuery('materialfield')->cond('FieldID', $obj->FieldID)->cond('locale', $obj->locale)->cond('MaterialID', $child)->first($child_mf)) {
+                            if (
+                            !dbQuery('materialfield')
+                                ->cond('FieldID', $obj->FieldID)
+                                ->cond('locale', $obj->locale)
+                                ->cond('MaterialID', $child)
+                                ->first($child_mf)
+                            ) {
                                 $child_mf = new \samson\activerecord\materialfield(false);
                                 $child_mf->MaterialID = $child;
                                 $child_mf->FieldID = $obj->FieldID;
@@ -190,62 +223,72 @@ class Field extends \samson\core\CompressableExternalModule implements \samson\c
                 $this->param = $param;
                 $this->save($value);
 
-			}
-			else e('CMSField - Entity ## with id: ## - does not exists', E_SAMSON_CORE_ERROR, array( $entity, $identifier) ); 
-		}	
-	}
-	
-	// Logic
-	
-	/**
-	 * Save input field value
-	 * @param mixed $value Field value
-	 */
-	public function save( $value )
-	{		
-		// Set field value
-		$this->obj[ $this->param ] = $value;
+            } else {
+                e(
+                    'CMSField - Entity ## with id: ## - does not exists',
+                    E_SAMSON_CORE_ERROR,
+                    array($entity, $identifier)
+                );
+            }
+        }
+    }
+
+    // Logic
+
+    /**
+     * Save input field value
+     * @param mixed $value Field value
+     */
+    public function save($value)
+    {
+        // Set field value
+        $this->obj[ $this->param ] = $value;
 
         // Create new event on object updating
         \samsonphp\event\Event::fire('samson.cms.input.change', array(& $this->obj));
 
-		// Save object
-		$this->obj->save();
-	}		
-	
-	/** @see \samson\core\iModuleViewable::toView() */
-	public function toView( $prefix = NULL, array $restricted = array() )
-	{	
-		// Generate unique prefix if not passed
-		$inner_prefix = 'field_';
-		
-		// Result view collection
-		$result = array();
-		
-// 		// Get only this class properties
-// 		$own = array_keys(get_object_vars($this));
-// 		$parent = array_keys(get_object_vars( ns_classname('ExternalModule','samson\core')));		
-// 		$properties = array_diff( $own, $parent );
-			
-		// Iterate object vars and gather them in collection	
-		foreach ( get_object_vars($this) as $k => $v ) $result[ $inner_prefix.$k ] = $v;			
-		
-		// Generate field action controller
-		$result[ $inner_prefix.'action' ] = url_build( \samson\core\AutoLoader::oldClassName(get_class($this)), $this->action );//'samson_cms_input_field'
-		
-		// Generate unique textarea id
-		$result[ $inner_prefix.'textarea_id' ] = 'field_'.$this->obj->id;
-		
-		// Render inner field view
-		$result[ $inner_prefix.'view' ] = $this->view($this->field_view)->set($result)->output();
-				
-		// Return input fields collection prepared for module view
-		return array( 
-			$prefix.'html' => $this
+        // Save object
+        $this->obj->save();
+    }
+
+    /** @see \samson\core\iModuleViewable::toView() */
+    public function toView($prefix = null, array $restricted = array())
+    {
+        // Generate unique prefix if not passed
+        $inner_prefix = 'field_';
+
+        // Result view collection
+        $result = array();
+
+    // 		// Get only this class properties
+    // 		$own = array_keys(get_object_vars($this));
+    // 		$parent = array_keys(get_object_vars(ns_classname('ExternalModule','samson\core')));
+    // 		$properties = array_diff($own, $parent);
+
+        // Iterate object vars and gather them in collection
+        foreach (get_object_vars($this) as $k => $v) {
+            $result[ $inner_prefix.$k ] = $v;
+        }
+
+        // Generate field action controller
+        $result[$inner_prefix.'action'] = url_build(
+            \samson\core\AutoLoader::oldClassName(get_class($this)),
+            $this->action
+        );//'samson_cms_input_field'
+
+        // Generate unique textarea id
+        $result[ $inner_prefix.'textarea_id' ] = 'field_'.$this->obj->id;
+
+        // Render inner field view
+        $result[ $inner_prefix.'view' ] = $this->view($this->field_view)->set($result)->output();
+
+        // Return input fields collection prepared for module view
+        return array(
+            $prefix.'html' => $this
                 ->view($this->default_view)
-				->set( $result )
-				->object($this->obj)
-			->output()
-		);		
-	}	
+                ->set($result)
+                ->object($this->obj)
+            ->output()
+        );
+    }
 }
--- www/field.js
+++ www/field.js
@@ -4,7 +4,7 @@
  * @param saveHandler External pointer to field saving
  * @constructor
  */
-var SamsonCMS_InputField = function( fields, saveHandler )
+var SamsonCMS_InputField = function(fields, saveHandler)
 {
 	// Pointer
 	var o = this;
@@ -13,10 +13,10 @@ var SamsonCMS_InputField = function( fields, saveHandler )
      * Click on input field handler and transform it to editable textarea
      * @param field SamsonJS object
      */
-    o.clickToEdit = function( field )
+    o.clickToEdit = function(field)
     {
         // Check if field is already opened
-        if( field.opened == undefined || field.opened == false )
+        if(field.opened == undefined || field.opened == false)
         {
             // Set flag that field is opened
             field.opened = true;
@@ -31,22 +31,22 @@ var SamsonCMS_InputField = function( fields, saveHandler )
             var tb = s('.__input',field);
 
             // No CSS for "file" input
-            if( tb.a('type') != 'file')
+            if(tb.a('type') != 'file')
             {
                 // Position and sizing
                 tb.css('position','absolute');
-                tb.left( of.left + 1);
-                tb.top( of.top + 1 );
-                tb.css( 'width',p.width()+'px');
-                tb.css( 'height', p.height()+'px');
-                tb.css( 'z-index', 99);
-                tb.css( 'padding-left', '5px');
-                tb.css( 'font-size', '1.6em');
-                tb.css( 'font-style', 'normal');
-                tb.css( 'left', '-10px');
-                tb.css( 'top', tb.val().length ? '-5px' : '-6px');
+                tb.left(of.left + 1);
+                tb.top(of.top + 1);
+                tb.css('width',p.width()+'px');
+                tb.css('height', p.height()+'px');
+                tb.css('z-index', 99);
+                tb.css('padding-left', '5px');
+                tb.css('font-size', '1.6em');
+                tb.css('font-style', 'normal');
+                tb.css('left', '-10px');
+                tb.css('top', tb.val().length ? '-5px' : '-6px');
                 tb.show();
-                tb.css( 'line-height', p.height()+'px');
+                tb.css('line-height', p.height()+'px');
                 tb.focus();
 
                 // Fix to put pointer at the end
@@ -60,119 +60,112 @@ var SamsonCMS_InputField = function( fields, saveHandler )
     /**
      * Field focus loose handler
      */
-    o.blur = function( field )
+    o.blur = function(field)
     {
 
     };
 
 	/** Init fields handler */
-	this.init = function( fields, saveHandler )
+	this.init = function(field, saveHandler)
 	{
-		/** Init */
-		fields.each(function( field )
-		{
-			// Create ajax loader object
-			var loader = new Loader( field.parent(), {type: 'absolute', top: 1, left: 1} );
+        // Create ajax loader object
+        var loader = new Loader(field.parent(), {type: 'absolute', top: 1, left: 1});
 
-			// Current value view
-			var sp = s( 'span', field );
+        // Current value view
+        var sp = s('span', field);
 
-			// Original value
-			var original = s( '.__hidden', field );
+        // Original value
+        var original = s('.__hidden', field);
 
-			// Get localized empty text
-			var empty_text = s('input[name="__empty_text"]',field).val();
+        // Get localized empty text
+        var empty_text = s('input[name="__empty_text"]',field).val();
 
-            // Bind click event
-            field.click( o.clickToEdit, false, true );
+        // Bind click event
+        field.click(o.clickToEdit, false, true);
 
-            field.keydown(function(elem, q, e){
-                if (e.keyCode == 13) {
-                    s('.__input', field).blur();
-                    e.preventDefault();
-                    return false;
-                }
-            });
+        field.keydown(function(elem, q, e){
+            if (e.keyCode == 13) {
+                s('.__input', field).blur();
+                e.preventDefault();
+                return false;
+            }
+        });
 
-			// Save handler
-			s('.__input', field ).blur( function(tb)
-			{
-				// Get current tb value
-				var new_value = tb.val();
+        // Save handler
+        s('.__input', field).blur(function(tb)
+        {
+            // Get current tb value
+            var new_value = tb.val();
 
-                // Eneble field editing flag
-                field.opened = false;
+            // Eneble field editing flag
+            field.opened = false;
 
-				// If value changed
-				if( new_value !== original.val() )
-				{
-					// Hide field view
-					sp.hide();
+            // If value changed
+            if(new_value !== original.val())
+            {
+                // Hide field view
+                sp.hide();
 
-					// Show loader
-					loader.show();
+                // Show loader
+                loader.show();
 
-					// Create form for async post
-					var form = s('<form method="post" enctype="multipart/form-data"></form>');
+                // Create form for async post
+                var form = s('<form method="post" enctype="multipart/form-data"></form>');
 
-					// Set field action as form action
-					form.a('action', s('input[name="__action"]', field).val());
+                // Set field action as form action
+                form.a('action', s('input[name="__action"]', field).val());
 
-					// Add all field hidden fields to form
-					s('input[type="hidden"]', field).each(function(hidden)
-					{
-						form.append('<textarea name="'+hidden.a('name')+'">'+hidden.val()+'</textarea>');
-					});
+                // Add all field hidden fields to form
+                s('input[type="hidden"]', field).each(function(hidden)
+                {
+                    form.append('<textarea name="'+hidden.a('name')+'">'+hidden.val()+'</textarea>');
+                });
 
-					// Add new field value
-					form.append('<textarea name="__value">'+new_value+'</textarea>');
+                // Add new field value
+                form.append('<textarea name="__value">'+new_value+'</textarea>');
 
-					// Perform ajax save
-					form.ajaxForm(function(responce)
-					{
-						// Hide loader
-						loader.hide();
+                // Perform ajax save
+                form.ajaxForm(function(responce)
+                {
+                    // Hide loader
+                    loader.hide();
 
-						// If external save handler is passed - call it
-						if( saveHandler ) saveHandler( responce, field, sp );
-						// Default behavior
-						else
-						{
-							// Think it is not empty
-							field.removeClass('__empty');
+                    // If external save handler is passed - call it
+                    if(saveHandler) saveHandler(responce, field, sp);
+                    // Default behavior
+                    else
+                    {
+                        // Think it is not empty
+                        field.removeClass('__empty');
 
-							// Set field view
-							sp.html( new_value );
+                        // Set field view
+                        sp.html(new_value);
 
-							// Fill sp with correct new value
-							if( !new_value.length  )
-							{
-								field.addClass('__empty');
-								sp.html( empty_text );
-							}
-						}
+                        // Fill sp with correct new value
+                        if(!new_value.length )
+                        {
+                            field.addClass('__empty');
+                            sp.html(empty_text);
+                        }
+                    }
 
-						// Save new value as original value
-						original.val( new_value );
+                    // Save new value as original value
+                    original.val(new_value);
 
-						// Show value view
-						sp.show();
-					});
-				}
-				else sp.show();
+                    // Show value view
+                    sp.show();
+                });
+            }
+            else sp.show();
 
-				// Destroy input
-				tb.hide();
-			});
-		});
-
-
+            // Destroy input
+            tb.hide();
+        });
 	};
 
 	// Perform object initialization
-	o.init( fields, saveHandler );
+	o.init(fields, saveHandler);
 };
 
-
-/** JS SamsonCMS Input field interaction on DOM ready */
-s('.__inputfield.__textarea').pageInit( SamsonCMS_InputField );
+// Bind input
+SamsonCMS_Input.bind(SamsonCMS_InputField, '.__inputfield.__textarea');
