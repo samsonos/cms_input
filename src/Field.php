86c6c1dd4b455e890e519c09ce32b034beb69ae4
omaximus <maxomelch93@gmail.com>
2015-04-02 17:07:56 +0300

    New method for fields generation added. Controller file renamed. Field rendering moved to Field class Views were corrected

--- src/Field.php
+++ src/Field.php
@@ -7,12 +7,11 @@ use samsonphp\event\Event;
 
 /**
  * Generic SamsonCMS input field
- * @author Vitaly Iegorov<egorov@samsonos.com>
- *
+ * @author Vitaly Iegorov <egorov@samsonos.com>
+ * @author Max Omelchenko <omelchenko@samsonos.com>
  */
 class Field
 {
-
     /** Database object class name which connected  with this field */
     protected $entity;
 
@@ -31,6 +30,12 @@ class Field
     /** @var null|dbQuery Pointer to activerecord dbQuery instance */
     protected $dbQuery = null;
 
+    /** Path to view file for field rendering */
+    protected $defaultView = "index";
+
+    /** Path to view file for inner field rendering */
+    protected $fieldView = "field";
+
     /**
      * Constructor
      *
@@ -44,9 +49,13 @@ class Field
         $this->dbQuery = $dbQuery;
         $this->param = isset($param) ? $param : $this->param;
 
+        // Check what has passed in $entity parameter
         if (!is_object($entity)) {
+            // If it's not object save it as entity
+            $this->entity = $entity;
+            // If identifier was passed try to create object from database object
+            // Otherwise show error
             if (isset($identifier)) {
-                $this->entity = $entity;
                 $this->dbObject = $this->dbQuery->className($entity)->id($identifier)->first();
             } else {
                 e(
@@ -69,9 +78,9 @@ class Field
      *
      * @return mixed CMS Field value
      */
-    public function getValue()
+    public function value()
     {
-        return $this->value;
+        return $this->dbObject[$this->param];
     }
 
     /**
@@ -95,13 +104,8 @@ class Field
         /** @var mixed $previousValue Previous instance value for transfer in event handlers */
         $previousValue = $this->dbObject[$this->param];
 
-        if ($this->param != 'numeric_value' && isset($this->dbObject['numeric_value'])) {
-            // Convert value to numeric value
-            $this->dbObject['numeric_value'] = $this->convert($value);
-        }
-
         // Set field value
-        $this->dbObject[$this->param] = $value;
+        $this->dbObject[$this->param] = $this->convert($value);
 
         // Create new event on object updating
         Event::fire('samson.cms.input.change', array(& $this->dbObject, $this->param, $previousValue));
@@ -111,17 +115,36 @@ class Field
     }
 
     /**
-     * Function to retrieve all object vars
+     * Function to render inner object
      *
-     * @param string $innerPrefix Prefix to form array keys
-     * @return array Object vars
+     * @param Application $renderer Renderer object
+     * @return string HTML string
      */
-    public function getObjectData($innerPrefix = '')
+    protected function viewField($renderer)
     {
-        $result = array();
-        foreach (get_object_vars($this) as $key => $value) {
-            $result[$innerPrefix . $key] = $value;
-        }
-        return $result;
+        return $renderer->view($this->fieldView)
+            ->set('fieldId', 'field_' . $this->dbObject->id)
+            ->set('value', $this->value())
+            ->output();
+    }
+
+    /**
+     * Function to render class
+     *
+     * @param Application $renderer Renderer object
+     * @param string $saveHandler Save controller name
+     * @return string HTML string
+     */
+    public function view($renderer, $saveHandler = 'save')
+    {
+        return $renderer->view($this->defaultView)
+            ->set('cssClass', $this->cssClass)
+            ->set('value', $this->value())
+            ->set('action', url_build($renderer->id(), $saveHandler))
+            ->set('entity', $this->entity)
+            ->set('param', $this->param)
+            ->set('objectId', $this->dbObject->id)
+            ->set('view', $this->viewField($renderer))
+            ->output();
     }
 }
